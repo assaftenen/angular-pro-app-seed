8c046f5a5378fc4b6d71ad2b5f69460153335953
Assaf Tenen <assaf.tenen@gmail.com>
2017-08-18 00:10:49 +0300

    workout module transitions

--- src/health/workouts/workouts.module.ts
+++ src/health/workouts/workouts.module.ts
@@ -1,25 +1,36 @@
-import { RouterModule, Routes } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-//containers
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SharedModule } from '../shared/shared.module';
+
+// components
+import { WorkoutFormComponent } from './components/workout-form/workout-form.component';
+import { WorkoutTypeComponent } from './components/workout-type/workout-type.component';
+
+// containers
 import { WorkoutsComponent } from './containers/workouts/workouts.component';
+import { WorkoutComponent } from './containers/workout/workout.component';
 
-export  const ROUTES: Routes= [
-    {path: '', component: WorkoutsComponent}
-]
-
-
+export const ROUTES: Routes = [
+  { path: '', component: WorkoutsComponent },
+  { path: 'new', component: WorkoutComponent },
+  { path: ':id', component: WorkoutComponent },
+];
 
 @NgModule({
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        RouterModule.forChild(ROUTES)
-    ],
-    declarations: [
-        WorkoutsComponent
-    ],
-   
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(ROUTES),
+    SharedModule
+  ],
+  declarations: [
+    WorkoutsComponent,
+    WorkoutComponent,
+    WorkoutFormComponent,
+    WorkoutTypeComponent
+  ]
 })
 export class WorkoutsModule {}
